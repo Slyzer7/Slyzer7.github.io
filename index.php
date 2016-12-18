8f2185a61a6ae4a0e62849696d9fdf74607885dc
Slyzer7 <dbsstudios3@yahoo.com>
2016-12-17 19:44:35 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<?php
+
+session_start();
+
+require 'database.php';
+
+if( isset($_SESSION['user_id']) ){
+
+	$records = $conn->prepare('SELECT id,email,password FROM users WHERE id = :id');
+	$records->bindParam(':id', $_SESSION['user_id']);
+	$records->execute();
+	$results = $records->fetch(PDO::FETCH_ASSOC);
+
+	$user = NULL;
+
+	if( count($results) > 0){
+		$user = $results;
+	}
+
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Welcome to your Web App</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href='http://fonts.googleapis.com/css?family=Comfortaa' rel='stylesheet' type='text/css'>
+</head>
+<body>
+
+	<div class="header">
+		<a href="index.php">Origin</a>
+	</div>
+
+	<?php if( !empty($user) ): ?>
+
+<style>
+/*----<?= $user['email']; ?>----*/
+</style>
+
+		<br />Welcome 
+		<br /><br />You are successfully logged in!
+		<br /><br />
+		<a href="home.php">Continue</a>
+			<p>Or</p>
+		<a href="logout.php">Logout</a>
+
+	<?php else: ?>
+
+		<h1>Please Login or Register</h1>
+		<a href="login.php">Login</a> or
+		<a href="register.php">Register</a>
+
+	<?php endif; ?>
+
+</body>
+</html>
